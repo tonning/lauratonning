b83bf3f3a4e48911b716e26b992eb1f6857baafe
Kristoffer Tonning <tonning@gmail.com>
2017-08-21 16:58:26 -0400

    Update mail address

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'tonning@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@kristoffertonning.com'),
         'name' => env('MAIL_FROM_NAME', 'Kristoffer Tonning'),
     ],
 
